05af0c15829747e683fbaa8a42751dd5f499d18e
loserchik67 <loserchik67@gmail.com>
2021-03-22 18:52:35 -0500

    Adds an array to the snake to hold body parts

--- snake.js
+++ snake.js
@@ -1,9 +1,21 @@
 export const SNAKE_SPEED = 1;
+const snakeBody = [{x: 10, y: 11}];
 
 export function update() {
-    console.log('snake');
+    for (let i = snakeBody.length - 2 ; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] };
+    }
+
+   // snakeBody[0].x += 0;
+   // snakeBody[0].y += 1;
 }
 
-export function draw() {
-    console.log('draw snake');
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.style.gridColumnStart = segment.x;
+        snakeElement.classList.add('snake');
+        gameBoard.appendChild(snakeElement);
+    })
 }
